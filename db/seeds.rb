d099a3da0229d1e5abee815a11d491279376c1cb
Remi Cordonnier <remi.cordo@gmail.com>
2019-11-18 18:16:01 +0100

    faking data garages with photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,14 @@
 
   puts "Creating new garages"
 
+Garage.destroy_all
+
 user = User.first
 
-Garage.create!(user: user, capacity: 3, price: 50, location: "98 rue kleber 59170 Croix", description: "Magnifique Garage plein sud")
-Garage.create!(user: user, capacity: 3, price: 50, location: "98 rue kleber 59170 Croix", description: "Magnifique Garage plein sud")
-Garage.create!(user: user, capacity: 3, price: 50, location: "98 rue kleber 59170 Croix", description: "Magnifique Garage plein sud")
-Garage.create!(user: user, capacity: 3, price: 50, location: "98 rue kleber 59170 Croix", description: "Magnifique Garage plein sud")
+10.times {
+g = Garage.create!(user: user, capacity: 3, price: 50, location: "98 rue kleber 59170 Croix", description: "Magnifique Garage plein sud")
+g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+g.save!
+}
 
 puts "finished"
