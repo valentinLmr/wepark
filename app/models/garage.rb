d6f1dd5e6029e1645ffd0d1175e668a3a09a3c04
Remi Cordonnier <remi.cordo@gmail.com>
2019-11-18 15:49:28 +0100

    model done

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,9 +1,11 @@
 class Garage < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   belongs_to :user
   validates :capacity, presence: true
   validates :price, presence: true
   validates :location, presence: true
   validates :description, presence: true
   validates :photo, presence: true
-  mount_uploader :photo, PhotoUploader
+
 end
