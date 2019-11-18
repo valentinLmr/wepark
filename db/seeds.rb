af69e0f56e30252fea2284857ee74a841e8e96e1
Remi Cordonnier <remi.cordo@gmail.com>
2019-11-18 18:35:41 +0100

    adding fake userwith db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-  puts "Creating new garages"
+puts "Creating new garages"
 
 Garage.destroy_all
 
 user = User.first
 
-10.times {
+6.times {
 g = Garage.create!(user: user, capacity: 3, price: 50, location: "98 rue kleber 59170 Croix", description: "Magnifique Garage plein sud")
 g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
 g.save!
 }
 
 puts "finished"
+
+
+puts "Creating new users"
+
+
+User.create!(email: 'delphine@gmail.com', password: 'azerty', first_name: 'Delphine', last_name: 'Cordonnier', phone_number: '0677889977')
+
+puts "Finised"
