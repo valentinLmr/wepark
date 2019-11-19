1216253461f418675eff54b6268954cf8ab34be8
Remi Cordonnier <remi.cordo@gmail.com>
2019-11-19 14:20:35 +0100

    resolving conflict

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,7 +1,7 @@
 class Rental < ApplicationRecord
   belongs_to :garage
   belongs_to :user
-  validates :start_date, :end_date, presence: true, availability: true
+  validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
 
   # validates :check_date
