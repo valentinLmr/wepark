e4c76e15e32a0c02428c50fad3ec72011d8de507
valentin lmr <valentinlmr28@gmail.com>
2019-11-19 13:51:43 +0100

    rentalss

--- app/validators/availability_garage_validator.rb
+++ app/validators/availability_garage_validator.rb
@@ -1,14 +1,2 @@
-puts "Je suis un Validator !!!!!!!!!!!!!!"
-class AvailabilityGarageValidator < ActiveModel::Validator
-  def validate_each(record, attribute, value)
-    rental = Rental.where(["user_id =?", record.garage_id])
-    date_ranges = rental.map { |r| r.start_date..r.end_date }
 
-    date_ranges.each do |range|
-      if range.include? value
-        record.errors.add(attribute, "not available")
-      end
-    end
-  end
-end
 
