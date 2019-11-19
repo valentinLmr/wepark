4373cfc3da45e74911bca8475b66aeba7ef744bc
Remi Cordonnier <remi.cordo@gmail.com>
2019-11-19 08:54:21 +0100

    feeding the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,41 @@ Garage.destroy_all
 
 user = User.first
 
-6.times {
-g = Garage.create!(user: user, capacity: 3, price: 50, location: "98 rue kleber 59170 Croix", description: "Magnifique Garage plein sud")
+g = Garage.create!(user: user, capacity: 10, price: 50, location: "01 rue kleber 59100 Marcq", description: "Magnifique Garage plein sud")
 g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
 g.save!
-}
+g = Garage.create!(user: user, capacity: 6, price: 30, location: "18 rue mouscron 59170 Lille", description: "Magnifique Garage plein sud")
+g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+g.save!
+g = Garage.create!(user: user, capacity: 7, price: 30, location: "99 rue kleber 45876 Croix", description: "Magnifique Garage plein sud")
+g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+g.save!
+g = Garage.create!(user: user, capacity: 6, price: 10, location: "98 rue kleber 09876 Lomme", description: "Magnifique Garage plein sud")
+g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+g.save!
+g = Garage.create!(user: user, capacity: 10, price: 100, location: "28 rue kleber 65432 Nice", description: "Magnifique Garage plein sud")
+g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+g.save!
+g = Garage.create!(user: user, capacity: 12, price: 20, location: "108 rue kleber 59170 Canne", description: "Magnifique Garage plein sud")
+g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+g.save!
+g = Garage.create!(user: user, capacity: 8, price: 30, location: "3248 rue kleber 75000 Paris", description: "Magnifique Garage plein sud")
+g.remote_photo_url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+g.save!
 
 puts "finished"
 
 
 puts "Creating new users"
 
-
-User.create!(email: 'delphine@gmail.com', password: 'azerty', first_name: 'Delphine', last_name: 'Cordonnier', phone_number: '0677889977')
+User.create!(email: 'delphine@gmail.com', password: 'azerty', first_name: 'Delphine', last_name: 'Cordonnier', phone_number: '0677333977')
+User.create!(email: 'remi@gmail.com', password: 'azerty', first_name: 'antoine', last_name: 'sorlin', phone_number: '0677889977')
+User.create!(email: 'antoine@gmail.com', password: 'azerty', first_name: 'Remi', last_name: 'Cordonnier', phone_number: '067711977')
+User.create!(email: 'martin@hotmail.com', password: 'azerty', first_name: 'Martin', last_name: 'Cordonnier', phone_number: '06723388977')
+User.create!(email: 'alex56@gmail.com', password: 'azerty', first_name: 'bouba', last_name: 'pfrt', phone_number: '0677889977')
+User.create!(email: 'duroyon@gmail.fr', password: 'azerty', first_name: 'Delphine', last_name: 'Cordonnier', phone_number: '0677889977')
+User.create!(email: 'Henripfr@gmail.com', password: 'azerty', first_name: 'Delphine', last_name: 'derujh', phone_number: '067788227')
+User.create!(email: 'Bouba@gmail.com', password: 'azerty', first_name: 'bouba', last_name: 'arlou', phone_number: '0677889977')
+User.create!(email: 'Sadju@gmail.com', password: 'azerty', first_name: 'Daju', last_name: 'Massoni', phone_number: '0611189977')
 
 puts "Finised"
