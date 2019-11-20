018d9b2fd509b59c987033bf012afb60c59b4153
Olivier Mathey <omathey@skynet.be>
2019-11-20 15:31:27 +0100

    fenetre info avec prix surfaceet phot et lien ok pas de border radius

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,8 +12,11 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this for infowindow
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this for infowindow
       .addTo(map);
   });
 };
